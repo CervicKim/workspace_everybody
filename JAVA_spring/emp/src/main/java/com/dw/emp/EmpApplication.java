15aecf40fc1780b2a0bc43b2069b3018f4d898a0
CervicKim <greeeeen0203@gmail.com>
2022-12-08 18:19:26 +0900

    221208 emp로 DB를 html로 꾸려서 열어보자

--- JAVA_spring/emp/src/main/java/com/dw/emp/EmpApplication.java
+++ JAVA_spring/emp/src/main/java/com/dw/emp/EmpApplication.java
@@ -1,10 +1,13 @@
 package com.dw.emp;
 
-public class EmpApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class EmpApplication{
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(EmpApplication.class,args);
 	}
-
+	
 }
--- JAVA_spring/emp/src/main/java/com/dw/emp/vo/EmpVO.java
+++ JAVA_spring/emp/src/main/java/com/dw/emp/vo/EmpVO.java
@@ -4,10 +4,8 @@ public class EmpVO {
 	
 	private int empno;		//사원번호
 	private String ename;		//사원이름
-	private String job;		//직책
-	private String hiredate;		//입사날짜
-	private String dname;		//부서이름
-	private String loc;		//부서위치
+	private int comm;
+	private int sal;
 	
 	public int getEmpno() {
 		return empno;
@@ -21,36 +19,18 @@ public class EmpVO {
 	public void setEname(String ename) {
 		this.ename = ename;
 	}
-	public String getJob() {
-		return job;
-	}
-	public void setJob(String job) {
-		this.job = job;
-	}
-	public String getHiredate() {
-		return hiredate;
-	}
-	public void setHiredate(String hiredate) {
-		this.hiredate = hiredate;
-	}
 	public int getSal() {
 		return sal;
 	}
 	public void setSal(int sal) {
 		this.sal = sal;
 	}
-	public String getDname() {
-		return dname;
+	public int getComm() {
+		return comm;
 	}
-	public void setDname(String dname) {
-		this.dname = dname;
+	public void setComm(int comm) {
+		this.comm = comm;
 	}
-	public String getLoc() {
-		return loc;
-	}
-	public void setLoc(String loc) {
-		this.loc = loc;
-	}
-	private int sal; // 급여
+	
 	
 }
